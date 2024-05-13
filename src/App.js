e6e9d687162b60cd8673d3d5c08723b1d86c37cc
hiyoons <arin9825@naver.com>
2024-05-13 16:41:30 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+
+} from "react-router-dom";
+
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+function App() {
+  return (<Router>
+    <Switch>
+      <Route path="/movie/:id">
+        <Detail />
+        </Route>
+      <Route path={`${process.env.PUBLIC_URL}/`} element={ <Home />}> 
+       
+        </Route>
+        
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
