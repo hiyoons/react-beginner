a43fae70279ef13cc58e316f6fdce5401f00fd26
hiyoons <arin9825@naver.com>
2024-05-13 23:08:43 +0900

    routepath 변경

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import {
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 function App() {
-  return (<Router>
+  return (<Router  basename={process.env.PUBLIC_URL}>
     <Switch>
-      <Route path={process.env.PUBLIC_URL+"/movie/:id"}>
+      <Route path={`${process.env.PUBLIC_URL}/movie/:id`}>
         <Detail />
         </Route>
-      <Route path={process.env.PUBLIC_URL + "/"} element={<Home />}> 
-       
+      <Route path={`${process.env.PUBLIC_URL}`} > 
+      <Home />
         </Route>
         
       </Switch>
